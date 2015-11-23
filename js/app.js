eb4df3ee487d3bd62e72cb7dfb84d374f2ded293
Marysia <mj.kociszewska@gmail.com>
2015-11-23 15:15:08 +0100

    Added hamburger menu

--- js/app.js
+++ js/app.js
@@ -4,5 +4,38 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 
+    //hamburger
+    function setMenu() {
+
+
+        function setHam(){
+            if ($(window).width() < 700) {
+                $('.header-nav').hide();
+                //$('.fusce').hide();
+                $('.vestibulum').hide();
+                $('.menuBtn').show();
+            } else {
+                $('.header-nav').show();
+                //$('.fusce').show();
+                $('.vestibulum').show();
+                $('.menuBtn').hide();
+                $('.mobileMenu').hide();
+            }
+        }
+        setHam();
+
+        $(window).on('resize', function() {
+            setHam();
+        });
+    }
+
+    function setClick() {
+        $('.menuBtn').on('click', function() {
+            $('.mobileMenu').toggle();
+        });
+    }
+
+    setMenu();
+    setClick();
 
     });
